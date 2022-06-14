f44e05fb89699691df359bf0d6b7ba6466733c44
GitCuaManhNe-Ahihi <shuriken8668@gmail.com>
2022-06-14 16:12:48 +0700

    getStaticPaths with ssg

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,11 +1,59 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { useRouter } from "next/router";
-import React from "react";
 
-type Props = {};
+type Props = {
+  post: {id:string,title:string, description:string,imageUrl:string};
+};
 
-export default function PostDetail({}: Props) {
+export default function PostDetail({ post }: Props) {
   const route = useRouter();
-
-  return <div>{JSON.stringify(route.query)}</div>;
+  if(!post) {
+    return null;
+  }
+  
+  return <>
+  <div>{post.id}</div>
+  <h1>{post.title}</h1>
+  <p>{post.description}</p>
+ <img src={post.imageUrl} alt={post.title} width={300} height={300} />
+  </>;
 }
 // DYNAMIC ROUTE
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  //server side
+  //build runtime
+  console.log("getStaticPath");
+  const res = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=10"
+  );
+  const { data } = await res.json();
+  return {
+    paths: data.map((item: any) => ({
+      params: { postId: item.id },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (
+  ctx: GetStaticPropsContext
+) => {
+  //server side
+  //build runtime
+  //hàm này gọi phía server có thể viết code server
+  if(!ctx.params?.postId){
+    return {notFound:true}
+  }
+  const res = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=10"
+  );
+  const { data } = await res.json();
+  const {id,title, description,imageUrl} = data?.filter((item: any) => (item.id === ctx.params?.postId))[0]
+
+  return {
+    props: {
+      post:{id,title, description,imageUrl} ,
+    },
+  };
+};
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {
@@ -11,7 +12,9 @@ export default function Index({posts}: Props) {
     <ul>
       {posts.map((post:any) => (
         <li key={post.id}>
-          <a href={`/post/${post.id}`}>{post.title}</a>
+          <Link  href={`/post/${post.id}`}>
+          <a>{post.title}</a>
+          </Link>
       </li>))}
     </ul>
     </>
@@ -24,8 +27,10 @@ export const getStaticProps: GetStaticProps<Props> = async (
   //server side
   //build runtime
   //hàm này gọi phía server có thể viết code server
+
   const res = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1&_limit=10')
   const {data} = await res.json()
+  console.log(data)
   return {
     props: {
       posts:data?.map((item:any) =>({id:item.id,title:item.title}))
