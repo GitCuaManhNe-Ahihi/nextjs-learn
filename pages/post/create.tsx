3ef984fe5fedf0a8a3d0842c2cae34d45e208c29
GitCuaManhNe-Ahihi <shuriken8668@gmail.com>
2022-06-14 21:25:13 +0700

    ssr cache s-maxage=5

--- pages/post/create.tsx
+++ pages/post/create.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { GetServerSideProps, GetServerSidePropsContext } from "next"
 
-type Props = {}
+type Props = {
+  query:any,
+  post:{title:string, author:string,description:string}
+}
 
-export default function Create({}: Props) {
+export default function Create(props: Props) {
   return (
-    <div>C</div>
+    <>
+    <p>Post detail</p>
+    <p>{props.post.title}</p>
+    <p>{props.post.author}</p>
+    <p>{props.post.description}</p>
+    </>
   )
+}
+export async function getServerSideProps(contex: GetServerSidePropsContext){
+  contex.res.setHeader('Cache-Control', 's-maxage=5') // 5 seconds
+  await new Promise((resolve,rejects) => setTimeout(()=>resolve(3000), 3000))
+  const postId = contex.query.postId
+  const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${postId}`)
+  const post = await response.json()
+  return {
+    props: {
+      query:contex.query,
+      post:post
+
+    }
+  }
 }
